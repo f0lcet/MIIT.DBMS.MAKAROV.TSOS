f3bd19087784ed460aba40f9fd0408a22984e4cb
f0lcet <164059208+f0lcet@users.noreply.github.com>
2025-01-15 17:07:56 +1000

    Add files via upload

---
+++ rbtree/rbtreePr/rbtreePr/rbtreePr.cpp
@@ -0,0 +1,43 @@
+﻿// C++ Program to Implement Red Black Tree
+
+#include <iostream>
+#include "rbtree.h"
+using namespace std;
+
+
+
+// Driver program to test Red-Black Tree
+int main()
+{
+    RedBlackTree<int> rbtree;
+
+    // Inserting values into Red-Black Tree
+    rbtree.insert(7);
+    rbtree.insert(3);
+    rbtree.insert(18);
+    rbtree.insert(10);
+    rbtree.insert(22);
+    rbtree.insert(8);
+    rbtree.insert(11);
+    rbtree.insert(26);
+    rbtree.insert(2);
+    rbtree.insert(6);
+
+    // Printing Red-Black Tree
+    rbtree.printTree();
+
+    // Deleting nodes from Red-Black Tree
+    cout << "After deleting 18:" << endl;
+    rbtree.remove(18);
+    rbtree.printTree();
+
+    cout << "After deleting 11:" << endl;
+    rbtree.remove(11);
+    rbtree.printTree();
+
+    cout << "After deleting 3:" << endl;
+    rbtree.remove(3);
+    rbtree.printTree();
+
+    return 0;
+}
